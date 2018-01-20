e6efe11daca6283ceba93cd54945fbb9ae22e2a3
Simber1 <simber2@btinternet.com>
2018-01-20 17:19:31 +0000

    Changes to be committed: 	modified:   Jediaplayer/src/net/prettyawful/JediaPlayer/Player.java  Changes not staged for commit: 	modified:   .gitignore

--- Jediaplayer/src/net/prettyawful/JediaPlayer/Player.java
+++ Jediaplayer/src/net/prettyawful/JediaPlayer/Player.java
@@ -24,11 +24,24 @@ public class Player  {
 
 	private JFrame frame;
 	private JLabel label;
-	private String song;
+	private static String song;
+	private MediaPlayer mediaPlayer;
+	private Media hit;
+	private int makeframemaybe;
+	
+
+
 
 
 	public Player() {
-		makeFrame();
+		if(makeframemaybe == 0) {
+			makeFrame();
+			makeframemaybe++;
+		}
+			
+		song = Player.songSelect().toString();
+		hit = new Media(new File(song).toURI().toString());  //Changing song into a form that Media can use
+		mediaPlayer = new MediaPlayer(hit);
 	}
 
 
@@ -38,13 +51,15 @@ public class Player  {
 		@SuppressWarnings("unused")
 		final JFXPanel fxPanel = new JFXPanel();  //Makes JavaFX not die https://stackoverflow.com/questions/14025718/javafx-toolkit-not-initialized-when-trying-to-play-an-mp3-file-through-mediap/43277386#43277386
 		
-		song = songSelect().toString();
+		
 		System.out.println("selecting");
 		
-		Media hit = new Media(new File(song).toURI().toString());  //Changing song into a form that Media can use
-		MediaPlayer mediaPlayer = new MediaPlayer(hit);  //Starting media player with track
+//		Media hit = new Media(new File(song).toURI().toString());  //Changing song into a form that Media can use
+//		MediaPlayer mediaPlayer = new MediaPlayer(hit);  //Starting media player with track
 		
-		mediaPlayer.setAutoPlay(true);
+		player.mediaPlayer.setVolume(0.05);
+		
+		player.mediaPlayer.setAutoPlay(true);
 
 
 
@@ -98,7 +113,7 @@ public class Player  {
 	}
 	
 	private void start(int stop) {
-
+		//Player player = new Player();
 		
 		if (stop ==0) {
 			song = songSelect().toString();
@@ -107,7 +122,7 @@ public class Player  {
 		}
 
 
-		mediaPlayer.setVolume(0.05);
+		
 		if (stop ==2) {
 			if(mediaPlayer.getStatus()!=MediaPlayer.Status.PLAYING) {
 					mediaPlayer.play();  //Playing media
@@ -224,7 +239,7 @@ public class Player  {
         
         item = new JMenuItem("About Clock...");
             item.addActionListener(new ActionListener() {
-                               public void actionPerformed(ActionEvent e) { showAbout(); }
+                               public void actionPerformed(ActionEvent e) { quit(); }
                            });
         menu.add(item);
 
